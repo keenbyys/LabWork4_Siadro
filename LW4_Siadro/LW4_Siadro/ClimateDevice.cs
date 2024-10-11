b4f259985cfc3c085637a3f9acea8d20824352a6
keenbyys <166899952+keenbyys@users.noreply.github.com>
2024-10-11 15:55:59 +0300

    1.0.019

---
+++ LW4_Siadro/LW4_Siadro/ClimateDevice.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LW4_Siadro
+{
+    internal class ClimateDevice
+    {
+        private double temperature;
+        private int humidity;
+        private int duration;
+
+        public double Temperature
+        {
+            get { return temperature; } 
+            set { if (value >= 10 && value <= 40)
+                    temperature = value;
+            }
+        }
+
+        public int Humidity
+        {
+            get { return humidity; }
+            set { if (value >= 20 && value <= 80)
+                        humidity = value;
+            }
+        }
+
+        public int Duration
+        {
+            get { return duration; }
+            set { if (value > 0)
+                duration = value; }
+        }
+
+        public ClimateDevice()
+        {
+            Temperature = 10;
+            Humidity = 30;
+        }
+
+        public ClimateDevice(double temperature, int humidity, int duration)
+        {
+            Temperature = temperature;
+            Humidity = humidity;
+            Duration = duration;
+        }
+
+        public virtual void TurnOn()
+        {
+            Console.WriteLine(" Device is turn on.");
+        }
+
+        public int CalculateEnergyUsage(int duration, int choice)
+        {
+            int watt = 40;
+            int calculateEnergyUsage = 0;
+            int i;
+
+            for (i = 1; i <= duration / 60; i++)
+            {
+                calculateEnergyUsage = watt * i;
+            }
+            
+            return calculateEnergyUsage;
+        }
+
+        public double CalculateEnergyUsage(double duration, double choice)
+        {
+            double wattHumidifier = 40.0;
+            double wattAirConditioner = 3000.0;
+            double wattHeater = 2500.0;
+            double calculateEnergyUsage = 0.0;
+            int i;
+            double countHours = 0.0;
+            double coefficient = 1.2;
+
+            for (i = 1; i <= duration / 60; i++)
+            {
+                countHours = i;
+            }
+
+            switch (choice)
+            {
+                case 1:
+                    calculateEnergyUsage = wattAirConditioner * countHours * coefficient;
+                    break;
+                case 2:
+                    calculateEnergyUsage = wattHeater * countHours * coefficient;
+                    break;
+                case 3:
+                    calculateEnergyUsage = wattHumidifier * countHours * coefficient;
+                    break;
+            }
+
+            return calculateEnergyUsage;
+        }
+
+        public void String()
+        {
+            Console.WriteLine(" {0}, {1}, {2}", Temperature, Humidity, Duration);
+        }
+    }
+}
---
+++ LW4_Siadro/LW4_Siadro/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LW4_Siadro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double temperature;
+            int humidity;
+            int duration;
+            int choice;
+            bool isInputCorrect = true;
+
+            Console.WriteLine("\n Welcome! Which device do you prefer to use?" +
+                "\n [1] Air conditioner" +
+                "\n [2] Heater" +
+                "\n [3] Air humidifier" +
+                "\n [4] Exit");
+            Console.Write("\n Your choice (number): ");
+
+            while (isInputCorrect)
+            {
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        ClimateDevice device = new ClimateDevice();
+                        device.String();
+                        device.TurnOn();
+                        int calculate1 = device.CalculateEnergyUsage(120, 1);
+                        Console.WriteLine(" {0} Watt", calculate1);
+                        double calculate2 = device.CalculateEnergyUsage(120, 1);
+                        Console.WriteLine(" {0} Watt", calculate2);
+                        break;
+                    case 2:
+                        break;
+                    case 3:
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.WriteLine("\n Oh, are you sure about that? (Т_Т)" +
+                            "\n [1] Yes" +
+                            "\n [2] No, can I go back in?");
+                        Console.Write("\n Your choice (number): ");
+
+                        switch (choice)
+                        {
+                            case 1:
+                                isInputCorrect = false;
+                                Console.WriteLine("\n The application is now complete.");
+                                break;
+                        }
+                        break;
+                    default:
+
+                        Console.Write(" Sorry, try again!" +
+                            " Your choice (number): ");
+                        continue;
+                }
+            }
+        }
+    }
+}
